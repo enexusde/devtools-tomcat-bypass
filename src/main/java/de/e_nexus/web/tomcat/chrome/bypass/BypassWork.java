89c8412d8696e66093b938201d0ee968ca7b605d
enexusde <github@e-nexus.de>
2020-03-19 10:04:48 +0100

    Improve documentation.

--- src/main/java/de/e_nexus/web/tomcat/chrome/bypass/BypassWork.java
+++ src/main/java/de/e_nexus/web/tomcat/chrome/bypass/BypassWork.java
@@ -2,6 +2,7 @@ package de.e_nexus.web.tomcat.chrome.bypass;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Serializable;
 
@@ -10,23 +11,47 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 /**
- * The order to bypass a file-request to the server.
+ * The order to bypass a local-file-request to the server.
+ * <p>
+ * Since this work is serializable it might be part of the http-session and
+ * might get passivated.
+ * 
+ * <p>
+ * Reactivation on a different system might cause a
+ * {@link FileNotFoundException}.
  */
 public final class BypassWork implements Serializable {
 
+	/**
+	 * The serial version uid.
+	 */
 	private static final long serialVersionUID = -679309896733271951L;
+
+	/**
+	 * The file to override.
+	 */
 	private final File overrideFile;
 
 	public BypassWork(File overrideFile) {
 		this.overrideFile = overrideFile;
 	}
 
+	/**
+	 * Bypass the local file into the http-request.
+	 * <p>
+	 * However if the original service might add some header this bypass does not.
+	 * 
+	 * @param request  The request, never <code>null</code>.
+	 * @param response The response, never <code>null</code>.
+	 * @throws IOException In the rare case that a request-socket has been closed
+	 *                     unexpectedly.
+	 */
 	public void doBypass(ServletRequest request, ServletResponse response) throws IOException {
 		try (FileInputStream fis = new FileInputStream(overrideFile)) {
 			byte[] tmp = new byte[1024];
 			int read = 0;
 			ServletOutputStream outputStream = response.getOutputStream();
-			while ((read = fis.read(tmp)) > -0) {
+			while ((read = fis.read(tmp)) > -1) {
 				outputStream.write(tmp, 0, read);
 			}
 		}
